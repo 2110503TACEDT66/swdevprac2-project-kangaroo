7133ace4864dabfa31b89067e39884f146905748
Aquaturner <paritleo@gmail.com>
2024-03-27 04:26:30 +0700

    Update getCar.tsx

--- libs/getCar.tsx
+++ libs/getCar.tsx
@@ -1,7 +1,9 @@
 export default async function getCar(id:string){
     const response = await fetch(`http://localhost:5050/api/v1/cars/${id}`);
+    
     if(!response.ok){
-        throw new Error("Fail to fetch cars")
+        throw new Error("Failed to fetch car")
     }
+
     return await response.json()
 } 

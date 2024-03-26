227e7adccbfb48cba2372fbef1d12fb6cfc8762e
Aquaturner <paritleo@gmail.com>
2024-03-26 22:57:20 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { Link as MuiLink } from "@mui/material";
 import CustomButton from "./CustomButton";
 import { IconButton } from "@mui/material";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
 
-export default function Navbar() {
+export default async function Navbar() {
+    const session = await getServerSession(authOptions)
+
     return (
         <header className="w-full absolute z-10">
             <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
@@ -17,12 +22,18 @@ export default function Navbar() {
                     <IconButton href="/booking" className="animate-fade-down animate-delay-500">
                         <DirectionsCarIcon sx={{ fontSize: 30 }}/>
                     </IconButton>
-                    
+
                     <IconButton href="/user" className="animate-fade-down animate-delay-500">
                         <AccountCircleIcon sx={{ fontSize: 30 }}/>
                     </IconButton>
 
-                    <CustomButton title="Sign In" btnType="button" containerStyles="text-base text-primary-blue rounded-full bg-white min-w-[130px] animate-fade-down animate-delay-1000"/>
+                    {
+                        session ?
+                        <CustomButton destination="/api/auth/signout" title="Sign Out" btnType="button" containerStyles="text-base text-primary-blue rounded-full bg-white min-w-[130px] animate-fade-down animate-delay-1000"/>
+                        : 
+                        <CustomButton destination="/api/auth/signin" title="Sign In" btnType="button" containerStyles="text-base text-primary-blue rounded-full bg-white min-w-[130px] animate-fade-down animate-delay-1000"/>
+                    }
+                    
                 </div>
             </nav>
         </header>

945cecb65704c625dd8ab4dd5861324eba2eefcf
Aquaturner <paritleo@gmail.com>
2024-03-26 18:44:45 +0700

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,16 +13,15 @@ export default function Hero() {
     return(
         <div className="hero">
             <div className="flex-1 pt-36 padding-x">
-                <h1 className="hero__title ">Find, book, or rent a car - quickly and easily!</h1>
-                <p className="hero__subtitle">Streamline your car rental experience with our effortless booking process.</p>
-                <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll}/>
+                <h1 className="hero__title animate-fade-right">Find, book, or rent a car - quickly and easily!</h1>
+                <p className="hero__subtitle animate-fade-right animate-delay-500">Streamline your car rental experience with our effortless booking process.</p>
+                <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10 animate-fade-up animate-delay-1000" handleClick={handleScroll}/>
             </div>
             <div className="hero__image-container">
-                <div className="hero__image">
+                <div className="hero__image animate-fade-left animate-delay-500">
                     <Image src="/benz.png" alt="hero" fill className="object-contain"/>
                     </div>
-                    <div className="hero__image-overlay"/>
-                
+                    <div className="hero__image-overlay animate-fade-left"/>
             </div>
         </div>
     );

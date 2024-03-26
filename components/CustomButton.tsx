7f9c1b67a7d67d2401cf24fa09e103b22a570d36
Aquaturner <paritleo@gmail.com>
2024-03-26 22:57:19 +0700

    Update CustomButton.tsx

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,11 +2,14 @@
 
 import { CustomButtonProps } from "@/types";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-export default function CustomButton({title, containerStyles, handleClick, btnType, textStyles,rightIcon, isDisabled} : CustomButtonProps) {
+export default function CustomButton({title, containerStyles, handleClick, btnType, textStyles,rightIcon, isDisabled, destination} : CustomButtonProps) {
+    const router = useRouter();
+
     return (
         <button
-        disabled={false} type={btnType || "button"} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
+        disabled={false} type={btnType || "button"} className={`custom-btn ${containerStyles}`} onClick={(e) => {e.stopPropagation(); router.push(`${destination}`)}}>
             <span className={`flex-1 ${textStyles}`}>{title}</span>
             {rightIcon && (
                 <div className="relative w-6 h-6">

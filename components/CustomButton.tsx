fb94637f3dd7f7abc90913d63fc62a9ce48907f1
tawanorkchiengtai <tawan.09@hotmail.com>
2024-03-27 01:26:08 +0700

    change CustonButton

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function CustomButton({title, containerStyles, handleClick, btnType, textStyles,rightIcon, isDisabled} : CustomButtonProps) {
     return (
         <button
-        disabled={false} type={btnType || "button"} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
+        disabled={isDisabled} type={btnType || "button"} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
             <span className={`flex-1 ${textStyles}`}>{title}</span>
             {rightIcon && (
                 <div className="relative w-6 h-6">

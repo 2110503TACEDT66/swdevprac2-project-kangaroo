48cbbda84b11b0c1d87923391ec481568a395494
Aquaturner <paritleo@gmail.com>
2024-03-27 04:08:19 +0700

    Update Booking.tsx

--- components/Booking.tsx
+++ components/Booking.tsx
@@ -1,52 +1,43 @@
 'use client'
 import { getCars } from "@/libs/getCars";
-import { CarItems } from "@/types";
+import { Booking, CarItems } from "@/types";
 import { useEffect, useState } from "react";
 import {BookingCard} from "@/components/BookingCard"
+import getBookings from "@/libs/getBookings";
 
-    export default function BookingLists() {
-    
-        const [cars, setCars] = useState<CarItems[]|null>(null);
-      
-          useEffect(() => {
-            fetchData();
-          }, []);
-        
-          const fetchData = async () => {
-            try {
-              const allCars = await getCars();
-              setCars(allCars.data);
-            } catch (error) {
-              console.error('Failed to fetch cars:', error);
-            }
-          };
-      
-      
-          // const allCars = await getCars({
-          //   manufacturer: searchParams.manufacturer || '',
-          //   year: searchParams.year || '',
-          //   limit: searchParams.limit || 10,
-          //   model: searchParams.model || ''
-          // })
-          // console.log(allCars)
-      
-        const isDataEmpty =   !cars || cars.length < 1;
-        
-        return (
-            <>
-              {!isDataEmpty ? (
-                <section>
-                 <div className="home__cars-wrapper">
-                    {cars?.map((car:any) => (<BookingCard car={car}/>))}
-                 </div>
-                </section>
-              ) : (
-                <div className="home__error-container">
-                  <h2 className="text-black text-xl font-bold ">Oops, no results</h2>
-                  {/* <p>{cars?.message}</p> */}
-                </div>
-              )}
-           </>
-        );
-      }
+export default function BookingLists(props: { token: string }) {
+  const [bookings, setBookings] = useState<Booking[]|null>(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const allBookings = await getBookings(props.token);
+      setBookings(allBookings.data);
+    } catch (error) {
+      console.error('Failed to fetch cars:', error);
+    }
+  };
+
+  const isDataEmpty = !bookings || bookings.length < 1;
+  
+  return (
+      <>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {bookings?.map((booking:any) => (<BookingCard booking={booking}/>))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold ">Oops, no results</h2>
+            {/* <p>{cars?.message}</p> */}
+          </div>
+        )}
+      </>
+  );
+}
 

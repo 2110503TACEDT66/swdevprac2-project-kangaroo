c63e26b579c13da0a31b936cd7407426c7fbe4dd
Aquaturner <paritleo@gmail.com>
2024-03-28 04:34:17 +0700

    Create ExtendDate.tsx

---
+++ components/ExtendDate.tsx
@@ -0,0 +1,102 @@
+"use client"
+import { Booking } from "@/types";
+import { useEffect,useState } from "react";
+import getBooking from "@/libs/getBooking";
+import Link from "next/link";
+import DateAdder from "@/utils/DateAdder";
+import updateBooking from "@/libs/updateBooking";
+
+export default function ExtendDate({ bookingID, token }: { bookingID: string, token: string }) {
+    const [booking, setBooking] = useState<Booking | null>(null);
+    const [extensionDays, setExtensionDays] = useState<number>(0);
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+        const singleBooking = await getBooking(bookingID, token);
+        setBooking(singleBooking.data);
+        } catch (error) {
+        console.error("Failed to fetch cars:", error);
+        }
+    };
+
+    const handleIncrease = () => {
+        setExtensionDays(prevDays => prevDays + 1);
+    };
+
+    const handleDecrease = () => {
+        if (extensionDays > 0) {
+            setExtensionDays(prevDays => prevDays - 1);
+        }
+    };
+
+    const handleConfirm = async () => {
+        if (booking && !isSubmitting) {
+            setIsSubmitting(true); 
+            const date = DateAdder(booking.bookingDateFrom, extensionDays)
+            try {
+                //console.log(DateAdder(booking.bookingDateFrom, extensionDays));
+                await updateBooking(booking._id, date, token);
+            } catch (error) {
+                console.error('Failed to update booking:', error);
+            } finally {
+                setIsSubmitting(false);
+            }
+        }
+    };
+
+    return (
+        <div className="mt-48 h-full w-1/3">
+            {
+            booking ? 
+            <div>
+                <div className="flex text-[24px] font-bold justify-between">
+                    <div className="flex flex-col">
+                        <div className="text-base text-zinc-500">From</div>
+                        {new Date(booking.bookingDateFrom).toLocaleDateString("en-US", {
+                            day: "numeric",
+                            month: "short",
+                            year: "numeric",
+                        })}
+                    </div>
+                    <div className="flex flex-col">
+                            <div className="text-base text-zinc-500">To</div>
+                            {new Date(booking.bookingDateTo).toLocaleDateString("en-US", {
+                            day: "numeric",
+                            month: "short",
+                            year: "numeric",
+                            })}
+                    </div>
+                </div>
+                <div className="flex mt-12 text-[24px] font-semibold justify-center">How many days would you like to extend?</div>
+                <div className="flex mt-6 justify-center">
+                    <button className="-translate-y-3 px-4 py-2 mr-2 text-gray-600 text-[48px]" onClick={handleDecrease}>
+                        &lt;
+                    </button>
+                    <div className="text-[48px] font-bold">{extensionDays}</div>
+                    <button className="-translate-y-3 px-4 py-2 ml-2 text-gray-600 text-[48px]" onClick={handleIncrease}>
+                        &gt;
+                    </button>
+                </div>
+                <div className="flex justify-center">
+                    <Link href="/booking">
+                        <button
+                            disabled={isSubmitting}
+                            className={`shadow bg-emerald-500 hover:bg-emerald-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded `}
+                            type="submit"
+                            onClick={handleConfirm}
+                        >
+                            Confirm
+                        </button>
+                    </Link>
+                </div>
+            </div>
+            : null
+            }   
+        </div>
+    )
+};

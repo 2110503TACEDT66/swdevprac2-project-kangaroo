21e769c1a49c1723fa39d97870b601803f0da1bb
Aquaturner <paritleo@gmail.com>
2024-03-27 20:16:28 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,12 +12,12 @@ export default async function Navbar() {
 
     return (
         <header className="w-full absolute z-10">
-            <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+            <nav className="max-w-[1440px] flex justify-between items-center sm:px-16 px-6 py-4">
                 <Link href="/" className="flex justify-center items-center animate-fade-down">
                     <Image src="/logo.svg" alt="Car Hub Logo" width={200} height={29} className="object-contain "/>
                 </Link>
 
-                <div className="flex gap-5">
+                <div className="flex items-center gap-5">
                     {
                         session ?
                         <IconButton href="/booking" className="animate-fade-down animate-delay-500" style={{ color: "#6667AB" }}>
@@ -33,13 +33,20 @@ export default async function Navbar() {
                         : null
                     }
                     {
+                        session ? null 
+                        : 
+                        <Link href="/signup">
+                            <button className="xl:text-primary-blue-100 text-primary-blue">Sign Up</button>
+                        </Link> 
+                    }
+                    {
                         session ?
                         <Link href="/api/auth/signout">
-                            <CustomButton title="Sign Out" btnType="button" containerStyles="text-base text-primary-blue rounded-full bg-white min-w-[130px] animate-fade-down animate-delay-1000"/>
+                            <CustomButton title="Sign Out" btnType="button" containerStyles="text-primary-blue rounded-full bg-white min-w-[130px] animate-fade-down animate-delay-1000"/>
                         </Link>
                         : 
                         <Link href="/api/auth/signin">
-                            <CustomButton title="Sign In" btnType="button" containerStyles="text-base text-primary-blue rounded-full bg-white min-w-[130px] animate-fade-down animate-delay-1000"/>
+                            <CustomButton title="Sign In" btnType="button" containerStyles="xl:text-primary-blue text-white rounded-full xl:bg-white bg-primary-blue min-w-[130px] animate-fade-down animate-delay-1000"/>
                         </Link> 
                     }
                 </div>

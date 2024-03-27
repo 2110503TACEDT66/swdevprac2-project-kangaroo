2da78cbf5f3646fb71140895431e727e52617857
Aquaturner <paritleo@gmail.com>
2024-03-28 04:34:06 +0700

    Delete layout.tsx

--- app/booking/extend/[cid]/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function ExtendLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="flex justify-center">
-            {children}
-        </div>
-    )
-};

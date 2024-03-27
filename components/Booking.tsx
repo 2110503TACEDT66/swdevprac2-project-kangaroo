76fcf3935ee54d845f410f8409561b11bd0a2da8
Aquaturner <paritleo@gmail.com>
2024-03-27 16:24:03 +0700

    Update Booking.tsx

--- components/Booking.tsx
+++ components/Booking.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 import { Booking, CarItems } from "@/types";
 import { useEffect, useState } from "react";
-import {BookingCard} from "@/components/BookingCard"
+import { BookingCard } from "@/components/BookingCard";
 import getBookings from "@/libs/getBookings";
 
 export default function BookingLists(props: { token: string }) {
-  const [bookings, setBookings] = useState<Booking[]|null>(null);
+  const [bookings, setBookings] = useState<Booking[] | null>(null);
 
   useEffect(() => {
     fetchData();
@@ -16,26 +16,32 @@ export default function BookingLists(props: { token: string }) {
       const allBookings = await getBookings(props.token);
       setBookings(allBookings.data);
     } catch (error) {
-      console.error('Failed to fetch cars:', error);
+      console.error("Failed to fetch cars:", error);
     }
   };
 
   const isDataEmpty = !bookings || bookings.length < 1;
-  
+
   return (
-      <>
-        {!isDataEmpty ? (
-          <section>
-            <div className="home__cars-wrapper">
-              {bookings?.map((booking:any) => (<BookingCard booking={booking} token={props.token}/>))}
-            </div>
-          </section>
-        ) : (
-          <div className="home__error-container">
-            <h2 className="text-black text-xl font-bold animate-bounce">Loading</h2>
+    <>
+      {!isDataEmpty ? (
+        <section>
+          <div className="home__cars-wrapper">
+            {bookings?.map((booking: any) => (
+              <BookingCard booking={booking} token={props.token} />
+            ))}
           </div>
-        )}
-      </>
+        </section>
+      ) : (
+        <div className="mt-16 flex justify-center items-center flex-row">
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent text-primary-blue"
+            role="loading"
+          >
+            <span className="hidden">Loading...</span>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
-

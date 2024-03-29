9ee7f911b04f3a03c10740bfa2afda8c032e6da4
Aquaturner <paritleo@gmail.com>
2024-03-29 10:15:14 +0700

    Update registerUser.tsx

--- libs/registerUser.tsx
+++ libs/registerUser.tsx
@@ -1,4 +1,5 @@
 import Swal from 'sweetalert2'
+import config from "../config"
 
 interface FormData {
     name: string;
@@ -9,7 +10,7 @@ interface FormData {
   }
 
 export default async function registerUser({name, tel, email, password, role}: FormData) {
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/auth/register`, {
+    const response = await fetch(`${config.backendUrl}/api/v1/auth/register`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"

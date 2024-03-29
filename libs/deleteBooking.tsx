f4e9d561c896f6ebf1e0374e9dd37b05ac9f1f82
Aquaturner <paritleo@gmail.com>
2024-03-29 10:14:59 +0700

    Update deleteBooking.tsx

--- libs/deleteBooking.tsx
+++ libs/deleteBooking.tsx
@@ -1,5 +1,7 @@
+import config from "../config"
+
 export default async function deleteBooking(bookingID: string, token: string) {
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/bookings/${bookingID}`, {
+    const response = await fetch(`${config.backendUrl}/api/v1/bookings/${bookingID}`, {
         method: "DELETE",
         headers: {
             authorization: `Bearer ${token}`

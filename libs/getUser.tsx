ff5ef528ad32aceadd1dccccb24a502ea9739623
Aquaturner <paritleo@gmail.com>
2024-03-29 10:15:10 +0700

    Update getUser.tsx

--- libs/getUser.tsx
+++ libs/getUser.tsx
@@ -1,5 +1,7 @@
+import config from "../config"
+
 export default async function getUser(token: string){
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/auth/me`, {
+    const response = await fetch(`${config.backendUrl}/api/v1/auth/me`, {
         method: "GET",
         headers: {
             authorization: `Bearer ${token}`,

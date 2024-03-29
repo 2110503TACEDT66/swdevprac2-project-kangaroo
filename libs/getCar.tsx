0a8a54de685740a4473229b90c4ff0ee26fc3578
Aquaturner <paritleo@gmail.com>
2024-03-29 10:15:05 +0700

    Update getCar.tsx

--- libs/getCar.tsx
+++ libs/getCar.tsx
@@ -1,5 +1,7 @@
+import config from "../config"
+
 export default async function getCar(id:string){
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/cars/${id}`);
+    const response = await fetch(`${config.backendUrl}/api/v1/cars/${id}`);
     
     if(!response.ok){
         throw new Error("Failed to fetch car")
